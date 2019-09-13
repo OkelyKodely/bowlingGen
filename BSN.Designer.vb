29b9b49a1e2bf61af5764cc2fff45f324e06fcec
Daniel Cho <dh.cho428@gmail.com>
2019-09-13 05:48:58 -0700

    Add files via upload

---
+++ BSN.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Bsn
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Bsn))
+        Me.lb1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lb1
+        '
+        Me.lb1.FormattingEnabled = True
+        Me.lb1.Location = New System.Drawing.Point(13, 13)
+        Me.lb1.Name = "lb1"
+        Me.lb1.Size = New System.Drawing.Size(189, 82)
+        Me.lb1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(586, 243)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(80, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(439, 246)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "How Many Sets (Max. 100)?"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(212, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(454, 200)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Generate Bowling Serial No.s"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(212, 245)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(155, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "How long serial no.s (Max. 15)?"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(367, 245)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(66, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(13, 134)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(189, 134)
+        Me.RichTextBox1.TabIndex = 6
+        Me.RichTextBox1.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button2.Location = New System.Drawing.Point(13, 102)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(189, 26)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Copy Sets"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Bsn
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(678, 280)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lb1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Bsn"
+        Me.Text = "Bowling Serial Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lb1 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ BSN.vb
@@ -0,0 +1,60 @@
+﻿Public Class Bsn
+
+
+    Private NUM_OF_PINS As Integer = 10
+    Dim rd As New Random
+
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            Return
+        End If
+
+        If CInt(TextBox1.Text) > 100 Then
+            TextBox1.Text = "100"
+        End If
+
+        If CInt(TextBox2.Text) > 15 Then
+            TextBox2.Text = "15"
+        End If
+
+        Dim nums As New List(Of UInteger)
+
+        Dim v = CInt(TextBox2.Text)
+
+        lb1.Items.Clear()
+
+        For k = 0 To CInt(TextBox1.Text) * NUM_OF_PINS - 1
+            Dim j As String = ""
+            For i As Integer = 0 To v - 1
+                Dim l = rd.Next(10)
+                j = j & l
+            Next
+            lb1.Items.Add(j)
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        RichTextBox1.
+            Text = ""
+
+        Dim v = 0
+
+        For k = 0 To lb1.Items.Count - 1
+            RichTextBox1.Text = RichTextBox1.Text & lb1.Items(k) & Environment.NewLine
+            v = v + 1
+            If v Mod 10 = 0 Then
+                RichTextBox1.Text = RichTextBox1.Text & Environment.NewLine
+            End If
+        Next
+
+    End Sub
+
+End Class
